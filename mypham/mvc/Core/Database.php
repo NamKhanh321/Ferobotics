898bb086432b91ef9a9c0133d6880dcff1021f4c
NamKhanh321 <129375652+NamKhanh321@users.noreply.github.com>
2024-12-24 02:33:44 +0700

    ok

--- mypham/mvc/Core/Database.php
+++ mypham/mvc/Core/Database.php
@@ -2,14 +2,16 @@
     class Database
     {
         public static $connect;
-        protected $severName = "localhost";
-        protected $userName = "root";
-        protected $passWord = "";
+        protected $severName = "mypham-server.mysql.database.azure.com";
+        protected $userName = "gkphkwttoo";
+        protected $passWord = "Nam123456";
         protected $dbName = "mypham";
 
         function __construct()
         {
-            self::$connect = mysqli_connect($this->severName,$this->userName,$this->passWord,$this->dbName);
+            self::$connect = mysqli_init();
+            mysqli_ssl_set(self::$connect,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+            mysqli_real_connect(self::$connect, $this->severName, $this->userName, $this->passWord, $this->dbName,3306,NULL,MYSQLI_CLIENT_SSL);
             mysqli_select_db(self::$connect,$this->dbName);
             mysqli_query(self::$connect, "SET NAMES 'utf8'");
         }
